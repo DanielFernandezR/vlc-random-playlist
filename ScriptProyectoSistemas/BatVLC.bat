8394779a7c7a360fc8bb4adeca8a61385c6ac827
Dual <Dual@TITAN07.sinf2003srvr2.local>
2019-11-07 16:40:32 +0100

    Cambio estructura Script VLC

--- ScriptProyectoSistemas/BatVLC.bat
+++ ScriptProyectoSistemas/BatVLC.bat
@@ -31,32 +31,61 @@ goto :mensaje
 :menu2
 echo -----------------------------------------------
 echo.
-echo     1.- Electro - Betty Boop
-echo     2.- Electro - Waiting For Love
-echo     3.- Electro - Animals
-echo     4.- Electro - Beautiful Now
-echo     5.- Electro - Find You
-echo     1.- Metal - Hail to the King
-echo     2.- Metal - The Sound Of Silence
-echo     3.- Metal - Through The Fire and Flames
-echo     4.- Metal - One
-echo     5.- Metal - Psychosocial
-echo     1.- Rock - I Write Sins Not Tragedies
-echo     2.- Rock - Thousand Foot Krutch
-echo     3.- Rock - My Demons
-echo     4.- Rock - So Far Away
-echo     5.- Rock - Nightmare
-echo     1.- Reggaeton - Runaway
-echo     2.- Reggaeton - Date la Vuelta
-echo     3.- Reggaeton - Que Tire Pa'lante
-echo     4.- Reggaeton - Con Calma
-echo     5.- Reggaeton - No me Conoce
+echo Selecciona el genero que quieres escuchar:
+echo.
+echo 1. Electro
+echo 2. Metal
+echo 3. Rock
+echo 4. Regaetton
 echo.
 echo -----------------------------------------------
-set /p genero="Elige el genero (e, m, r, rt) que quieras escuchar: "
-set /p numcancion="Elige el numero de una cancion: "
+set /p generonum="Escribe el numero de una de las opciones propuestas: "
+
+cls
+goto genero%generonum%
+
+:genero1
+echo.
+echo -----------------------------------------------------------------
+dir %ruta%\*.mp3 | find "e_0"
+echo -----------------------------------------------------------------
+echo.
+set /p numcancion="Elige el numero de la cancion: "
 set vlc="C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
-%vlc% "%ruta%"\%genero%_0%numcancion%_*.mp3
+%vlc% "%ruta%"\e_0%numcancion%_*.mp3
+goto :mensaje
+
+:genero2
+echo.
+echo -----------------------------------------------------------------
+dir %ruta%\*.mp3 | find "m_0"
+echo -----------------------------------------------------------------
+echo.
+set /p numcancion="Elige el numero de la cancion: "
+set vlc="C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+%vlc% "%ruta%"\m_0%numcancion%_*.mp3
+goto :mensaje
+
+:genero3
+echo.
+echo -----------------------------------------------------------------
+dir %ruta%\*.mp3 | find "r_0"
+echo -----------------------------------------------------------------
+echo.
+set /p numcancion="Elige el numero de la cancion: "
+set vlc="C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+%vlc% "%ruta%"\r_0%numcancion%_*.mp3
+goto :mensaje
+
+:genero4
+echo.
+echo -----------------------------------------------------------------
+dir %ruta%\*.mp3 | find "rt_0"
+echo -----------------------------------------------------------------
+echo.
+set /p numcancion="Elige el numero de la cancion: "
+set vlc="C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+%vlc% "%ruta%"\rt_0%numcancion%_*.mp3
 goto :mensaje
 
 :menu3
@@ -71,5 +100,6 @@ exit
 :mensaje
 echo Presiona una tecla para volver al menu...
 pause > Nul
+cls
 goto menu
 
