053177879bd114deddf98e4519af0797bdef2665
Dual <Dual@TITAN07.sinf2003srvr2.local>
2019-11-07 18:49:32 +0100

    Estructura del archivo random y test creado

---
+++ logica_proyecto/random_playlist_generator.py
@@ -0,0 +1,11 @@
+from random import randrange
+
+def generate_random_playlist(diccionario):
+    assert isinstance(diccionario, dict)w<
+    lista_canciones = []
+
+
+
+
+    assert len(lista_canciones) == len(diccionario)
+    return lista_canciones[]
---
+++ logica_proyecto/test_random_playlist_generator.py
@@ -0,0 +1,5 @@
+from random_playlist_generator import generate_random_playlist
+
+
+def test_devolver_lista_bien():
+    assert generate_random_playlist()
