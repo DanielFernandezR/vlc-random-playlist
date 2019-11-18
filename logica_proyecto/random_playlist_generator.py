592b759ea2dd52dbaa81204179eaa438e83a5351
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-11-18 12:36:44 +0100

    feat: Crear una lista con canciones y numeros aleatorios
    He creado una lista con "numero aleatorio", "cancion, con ese orden,
    pero falla el import de api

--- logica_proyecto/random_playlist_generator.py
+++ logica_proyecto/random_playlist_generator.py
@@ -1,26 +1,16 @@
 from random import randrange
+from acceso_a_datos.api import get_name_dict
 
 
-def generate_random_playlist(diccionario):
+def generate_random_playlist():
+    diccionario = get_name_dict()
     assert isinstance(diccionario, dict)
     lista_canciones = []
-    if len(lista_canciones) != len(diccionario):
-        nombre_canciones = list(diccionario.keys())
-        num_aleatorio = randrange(1, 3)
-        lista_canciones += str(num_aleatorio)
-        lista_canciones += str(nombre_canciones(0,-1))
+    nombre_canciones = list(diccionario.keys())
+    for cancion in nombre_canciones:
+        while cancion not in lista_canciones:
+            num_aleatorio = randrange(1, 4)
+            if str(num_aleatorio) not in lista_canciones:
+                lista_canciones += str(num_aleatorio)
+                lista_canciones.append(cancion)
     return lista_canciones
-
-
-if __name__ == "__main__":
-
-    assert generate_random_playlist({"California_Uber_Alles.mp3":
-                                     {"track-number": 3, "artist": "Dead Kennedys", "album": "Dead Kennedys",
-                                         "location": "./biblioteca/California_Uber_Alles.mp3"},
-                                     "Seattle_Party":
-                                     {"track-number": 1, "artist": "Chastity Belt", "album": "No regrets",
-                                      "location": "./biblioteca/Seattle_Party.flac"},
-                                     "King_Kunta":
-                                     {"track-number": 3, "artist": "Kendrick Lamar",
-                                      "album": "To Pimp A Butterfly", "location": "./biblioteca/King_Kunta.mp3"}
-                                     }) == True
