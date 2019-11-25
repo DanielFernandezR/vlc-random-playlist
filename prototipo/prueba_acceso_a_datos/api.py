7b033d9694d14c11e08fe368623505073b1f2938
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-11-25 11:25:59 +0100

    feat: Creación módulo contar elementos XML
    Para hacer un assert en el módulo api, hemos creado un módulo que se
    encarga de contar los elementos que hay en el XML.

--- prototipo/prueba_acceso_a_datos/api.py
+++ prototipo/prueba_acceso_a_datos/api.py
@@ -1,4 +1,5 @@
 from prueba_acceso_a_datos.check_XML import check_XML
+from prueba_logica_proyecto.comprobar_long_dict import contar_canciones_diccionario
 
 
 def get_name_and_path_dict():
@@ -9,5 +10,5 @@ def get_name_and_path_dict():
             nombre_ruta = {}
             nombre_ruta[cancion.find('name').text] = cancion.find("path").text
             diccionario.update(nombre_ruta)
-    assert len(diccionario) == 20
+    assert len(diccionario) == contar_canciones_diccionario()
     return diccionario
