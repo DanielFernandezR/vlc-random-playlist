4966ec398c6efb1a21a0c4400b46c88084658776
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-11-27 17:20:12 +0100

    refactor: Refactorizacion del modulo execute_vlc
    Hemos añadido un try-except-finally al modulo execute_vlc y funciona

--- presentacion/execute_vlc.py
+++ presentacion/execute_vlc.py
@@ -4,9 +4,12 @@ from logica_proyecto.juntar_dos_listas import fusion_listas
 
 
 def ejecuta_vlc():
-    ruta_vlc = "‪‪C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
     ruta_cancion = " ".join(fusion_listas())
-
-    #añadir_path = subprocess.Popen("set PATH=" + os.environ["PATH"] + ";C:\Program Files (x86)\VideoLAN\VLC\;")
-    ejecutar = subprocess.Popen(r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe " + ruta_cancion)
+    try:
+        ejecutar = subprocess.Popen("vlc" + ruta_cancion)
+    except FileNotFoundError:
+        print("El programa VLC no esta asociado a la variable PATH")
+    finally:
+        ejecutar = subprocess.Popen(r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe " + ruta_cancion)
+# añadir_path = subprocess.Popen("set PATH=" + os.environ["PATH"] + ";C:\Program Files (x86)\VideoLAN\VLC\;")
     return ejecutar
--- test_random_playlist_generator.py
+++ test_random_playlist_generator.py
@@ -1,15 +1,7 @@
-from prueba_acceso_a_datos.api import *
-from prueba_logica_proyecto.comprobar_long_dict import *
-from prueba_ejecutar_vlc.execute_vlc import *
+from acceso_a_datos.api import *
+from logica_proyecto.comprobar_long_dict import *
+from presentacion.execute_vlc import *
 
 
 def test_longitud_dict_igual_num_canciones_xml():
     assert len(get_name_and_path_dict()) == contar_canciones_xml()
-
-
-def test_todas_las_canciones_dict_en_lista():
-    assert 
-
-
-def test_canciones_no_se_repitan_en_dict():
-    assert 
