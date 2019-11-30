6b927cea89801b3d29340920e7889775bd56fdcd
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-11-30 20:49:18 +0100

    feat: Nuevo try except en ejecutar_vlc
    Hemos añadido un nuevo try except para probar las 2 opciones de abrir el
    vlc

--- presentacion/ejecutar_vlc.py
+++ presentacion/ejecutar_vlc.py
@@ -7,5 +7,14 @@ def ejecuta_vlc(lista_canciones, RUTA_VLC):
         vlc_en_path = subprocess.Popen("vlc " + lista_canciones)
     except FileNotFoundError:
         print("El programa VLC no esta asociado a la variable PATH")
+    else:
+        exit()
+
+    try:
         vlc_ruta_completa = shlex.split(RUTA_VLC + " " + lista_canciones, posix=False)
         vlc_en_path = subprocess.Popen(vlc_ruta_completa)
+    except FileNotFoundError:
+        print("El programa VLC no esta en la ruta C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe")
+        exit()
+    else:
+        exit()
