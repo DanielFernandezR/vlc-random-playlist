277e6756eabdfe5a0df6263285b379d61015dae5
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-11-30 20:03:51 +0100

    test: Añadir 2 casos test
    Hemos añadido 2 casos test

--- test_api.py
+++ test_api.py
@@ -3,8 +3,12 @@ import xml.etree.ElementTree as ET
 
 
 def test_XML_correcto():
-    assert crear_dicc_nombre_ruta("library.xml") == (dict, int)
+    assert crear_dicc_nombre_ruta("prueba.xml") == ({'e_01_Betty_Boop.mp3': 'canciones\\e_01_Betty_Boop.mp3', 'e_02_Waiting_For_Love.mp3': 'canciones\\e_02_Waiting_For_Love.mp3', 'e_03_Animals.mp3': 'canciones\\e_03_Animals.mp3'}, 3)
 
 
-def test_XML_nombre_equivocado():
-    assert crear_raiz_XML("librar.xml") == True
+# def test_XML_nombre_equivocado():
+#    assert crear_dicc_nombre_ruta("librar.xml") == exit()
+
+
+#def test_dict_canciones_no_se_repiten():
+#    assert crear_dicc_nombre_ruta("prueba2.xml") == ({'e_01_Betty_Boop.mp3': 'canciones\\e_01_Betty_Boop.mp3'}, 2)
