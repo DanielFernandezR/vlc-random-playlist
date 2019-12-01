fb567f2908d85247cddba512dae13204e9e2a654
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-12-01 21:28:25 +0100

    refactor: Cambio menor explicación
    Cambio de una palabra en una frase

--- test_api.py
+++ test_api.py
@@ -6,7 +6,7 @@ def test_XML_correcto_y_long_dict_igual_num_canciones_xml():
                                                      'e_02_Waiting_For_Love.mp3': 'canciones\\e_02_Waiting_For_Love.mp3',
                                                      'e_03_Animals.mp3': 'canciones\\e_03_Animals.mp3'}, 3)
 
-# Este caso test solo funciona si quitamos la precondición de crear_dicc_nombre_ruta()
+# Este caso test solo funciona si quitamos la postcondición de crear_dicc_nombre_ruta()
 # def test_dict_canciones_no_se_repiten():
 #    assert crear_dicc_nombre_ruta("archivos_xml\\prueba2.xml") == ({'e_01_Betty_Boop.mp3': 'canciones\\e_01_Betty_Boop.mp3'}, 2)
 
